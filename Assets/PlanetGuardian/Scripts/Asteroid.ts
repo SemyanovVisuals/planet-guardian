09b5d2ca354460074ff28cc7b4d720a0739b7399
DeanCode <ds@xxxmail.eu>
2026-01-31 09:01:16 +0100

    Added particles

--- Assets/PlanetGuardian/Scripts/Asteroid.ts
+++ Assets/PlanetGuardian/Scripts/Asteroid.ts
@@ -3,6 +3,7 @@ import animate from "SpectaclesInteractionKit.lspkg/Utils/animate"
 @component
 export class Asteroid extends BaseScriptComponent {
     @input rotationSpeed : number = 0.1
+    @input orbitRadius : number = 25.0
 
     private rotation : vec3
 
@@ -17,14 +18,49 @@ export class Asteroid extends BaseScriptComponent {
             ended: null,
         })
         */
-        this.getTransform().setLocalScale(vec3.one().uniformScale(Math.random() * 0.5 + 0.5))
+
+        
+        const dir = vec3.randomDirection().mult(new vec3(1, 0.1, 1)).normalize();
+        const pos = dir.uniformScale(this.orbitRadius);
+        const randomOffset = vec3.randomDirection().uniformScale(3);
+
+       // prefab.getTransform().setLocalPosition(pos.add(randomOffset));
+
+        const finalScale = vec3.one().uniformScale(Math.random() * 0.5 + 0.5);
+
+        // Orbit animation
+        animate({
+            easing: "ease-out-sine",
+            duration: 10,
+            update: (t: number) => {
+                this.getTransform().setLocalScale(vec3.lerp(vec3.zero(), finalScale, t))
+
+                const enterOrbit = dir.uniformScale((1 - Math.sqrt(t)) * 40);
+   
+                this.getTransform().setLocalPosition(pos.add(randomOffset).add(enterOrbit));
+            },
+            ended: null,
+        })
+
         this.createEvent("UpdateEvent").bind(this.update.bind(this))
-        this.rotation = vec3.randomDirection()
+        this.rotation = vec3.randomDirection().uniformScale(Math.random() * 2 + 1)
+    }
+
+    public enterOrbit() {
+        const startPos = this.getTransform().getLocalPosition();
+
+        animate({
+            easing: "ease-out-sine",
+            duration: 5,
+            update: (t: number) => {
+                this.getTransform().setLocalPosition(vec3.lerp(startPos, vec3.zero(), t));
+            },
+            ended: null,
+        })
     }
 
     private update() {
         const transform = this.getTransform();
-        // idk how to multiply by scalar value :/
         const rotation = quat.fromEulerVec(this.rotation.uniformScale(getTime() * this.rotationSpeed));
         transform.setLocalRotation(rotation);
     }
--- Assets/PlanetGuardian/Scripts/Orbit.ts
+++ Assets/PlanetGuardian/Scripts/Orbit.ts
@@ -19,25 +19,7 @@ export class Orbit extends BaseScriptComponent {
         console.log("Spawning asteroid in: " + this.getSceneObject().name);
 
         const prefab = this.asteroidPrefab.instantiate(this.getSceneObject());
-        
-        const dir = vec3.randomDirection().mult(new vec3(1, 0, 1)).normalize();
-        const pos = dir.uniformScale(this.orbitRadius);
-        const randomOffset = vec3.randomDirection().uniformScale(3);
-
-        prefab.getTransform().setLocalPosition(pos.add(randomOffset));
-
-        // Scale-in animation
-        /*animate({
-        //    easing: "ease-in-elastic",
-            duration: 5,
-            update: (t: number) => {
-                this.getTransform().setLocalScale(vec3.lerp(vec3.zero(), vec3.one(), t))
-   
-                const randomPos = randomDir.uniformScale(this.orbitRadius);
-                prefab.getTransform().setLocalPosition(randomPos);
-            },
-            ended: null,
-        })*/
+        // TODO: There must be a way to access the Asteroid component from prefab
     }
 
     private update() {
