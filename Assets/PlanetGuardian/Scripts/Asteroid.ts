1aaa55577ad2ce84ebd0acdb59e164d7d77d9102
DeanCode <ds@xxxmail.eu>
2026-01-31 16:09:13 +0100

    Changed asteroid animation

--- Assets/PlanetGuardian/Scripts/Asteroid.ts
+++ Assets/PlanetGuardian/Scripts/Asteroid.ts
@@ -1,4 +1,4 @@
-import animate from "SpectaclesInteractionKit.lspkg/Utils/animate"
+import animate, {CancelSet} from "SpectaclesInteractionKit.lspkg/Utils/animate"
 
 @component
 export class Asteroid extends BaseScriptComponent {
@@ -8,33 +8,44 @@ export class Asteroid extends BaseScriptComponent {
     @input orbitRadius : number = 25.0
     
     private rotation : vec3
+    private finalScale : vec3
+    private finalPos : vec3
+    private dir : vec3
+    private spawnTime : number
 
     onAwake() {
-        const dir = vec3.randomDirection().mult(new vec3(1, 0.1, 1)).normalize();
-        const pos = dir.uniformScale(this.orbitRadius);
+        this.dir = vec3.randomDirection().mult(new vec3(1, 0.1, 1)).normalize();
+        const pos = this.dir.uniformScale(this.orbitRadius);
         const randomOffset = vec3.randomDirection();
-        const finalPos = pos.add(randomOffset);
-        const finalScale = vec3.one().uniformScale(Math.random() * 0.5 + 0.5);
+        this.finalPos = pos.add(randomOffset);
+        this.finalScale = vec3.one().uniformScale(Math.random() * 0.5 + 0.5);
+        this.spawnTime = getTime();
 
         // Orbit animation
-        animate({
+        /*animate({
             easing: "ease-out-sine",
             duration: 10,
             update: (t: number) => {
                 const enterOrbit = dir.uniformScale((1 - Math.sqrt(t)) * 40);
    
-                this.getTransform().setLocalScale(vec3.lerp(vec3.zero(), finalScale, t))
-                this.getTransform().setLocalPosition(finalPos.add(enterOrbit));
+                this.getTransform()?.setLocalScale(vec3.lerp(vec3.zero(), finalScale, t))
+                this.getTransform()?.setLocalPosition(finalPos.add(enterOrbit));
             },
             ended: null,
-        })
+            cancelSet: this.cancelSet,
+        })*/
 
         this.createEvent("UpdateEvent").bind(this.update.bind(this))
         this.rotation = vec3.randomDirection().uniformScale(Math.random() * 2 + 1)
         
         // This is so that the flame particles are moving in the correct direction
         this.getTransform()
-            .setLocalRotation(quat.rotationFromTo(vec3.zero(), finalPos))
+            .setLocalRotation(quat.rotationFromTo(vec3.zero(), this.finalPos))
+    }
+
+    public onDestroy() {
+       // this.cancelSet.cancel();
+        this.sceneObject.destroy();
     }
 
     public enterPlanet() {
@@ -54,10 +65,15 @@ export class Asteroid extends BaseScriptComponent {
         })
     }
 
-    public test() { return 1 }
-
     private update() {
+        const timePast = Math.min((getTime() - this.spawnTime) / 10.0, 1.0);
+        const t = Math.sin(timePast * (Math.PI / 2));
         const rotation = quat.fromEulerVec(this.rotation.uniformScale(getTime() * this.rotationSpeed));
         this.model.getTransform().setLocalRotation(rotation);
+
+        const enterOrbit = this.dir.uniformScale((1 - Math.sqrt(t)) * 40);
+   
+        this.getTransform().setLocalScale(vec3.lerp(vec3.zero(), this.finalScale, t))
+        this.getTransform().setLocalPosition(this.finalPos.add(enterOrbit));
     }
 }
