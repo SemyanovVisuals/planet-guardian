17e33e5e4f8afaab61426ac5a51395317837267e
SemyanovVisuals <katia.semyanovskaya@gmail.com>
2026-02-01 01:32:15 +0100

    EXPERIMENTAL: Rocket & Asteroid collision handler
    TODO: check if works (Rocket script)

--- Assets/PlanetGuardian/Scripts/Rocket.ts
+++ Assets/PlanetGuardian/Scripts/Rocket.ts
@@ -6,27 +6,65 @@ export class Rocket extends BaseScriptComponent {
     //private startTime: number;
 
     onAwake() {
-        this.sceneObject.getComponent("Physics.ColliderComponent").onCollisionEnter.add((arg) => {
-            const asteroid = this.isAsteroid(arg.collision.collider.sceneObject);
+        // this.sceneObject.getComponent("Physics.ColliderComponent").onCollisionEnter.add((arg) => {
+        //     const asteroid = this.findAsteroidComponent()
             
-            this.onDestroy();
-            if (asteroid) {
-                asteroid.onDestroy(null);
-            }
-        });
+        //     this.onDestroy();
+        //     if (asteroid) {
+        //         asteroid.onDestroy(null);
+        //     }
+        // });
+
+        const collider = this.sceneObject.getComponent("Physics.ColliderComponent")
+
+        collider.onOverlapEnter.add((e: OverlapEnterEventArgs) => {
+            // this.onFingerOverlapEnter(e, isLeftHand)
+            this.onRocketOverlapEnter(e)
+        })
+
         this.createEvent("UpdateEvent").bind(this.update.bind(this));
-        setTimeout(this.onDestroy.bind(this), 10_000);
+        setTimeout(this.onDestroyRocket.bind(this), 10_000);
     } 
 
-    isAsteroid(obj : SceneObject): Asteroid {
-        for (const script of obj.getComponents("Component.ScriptComponent")) {
-            if (script.getTypeName().toString() == Asteroid.getTypeName().toString())
-                return script as any as Asteroid;
+    onRocketOverlapEnter(e: OverlapEnterEventArgs) {
+        const overlappedObject = e.overlap.collider.getSceneObject()
+
+        print(`Rocket: detected overlap with ${overlappedObject.name}`)
+
+        // Check if this object has a GrabbableObject component
+        const asteroid = this.findAsteroidComponent(overlappedObject)
+
+        if(asteroid) {
+            // ROCKET HIT ASTEROID: destroy both
+            asteroid.onDestroyAsteroid()
+            this.onDestroyRocket()
         }
-        return null;
     }
 
-    onDestroy() {
+    // isAsteroid(obj : SceneObject): Asteroid {
+    //     for (const script of obj.getComponents("Component.ScriptComponent")) {
+    //         if (script.getTypeName().toString() == Asteroid.getTypeName().toString())
+    //             return script as any as Asteroid;
+    //     }
+    //     return null;
+    // }
+
+    /**
+   * Find Asteroid component on a scene object
+   */
+    private findAsteroidComponent(sceneObject: SceneObject): Asteroid | null {
+        const allComponents = sceneObject.getComponents("Component.ScriptComponent")
+        for (let i = 0; i < allComponents.length; i++) {
+            const comp = allComponents[i]
+            // Check if this is a TriggerObject by checking if it has the required methods
+            if (comp && typeof (comp as any).enterPlanet === "function") {
+                return comp as Asteroid
+            }
+        }
+        return null
+    }
+
+    onDestroyRocket() {
         this.sceneObject.destroy();
     }
 
